4c90fc007f8f64708c95a0ad5c890a935e552618
regan <reganlperkins@gmail.com>
2019-07-16 20:39:35 +0000

    use process.env for github token

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,6 @@
+require('dotenv').config();
 const request = require('request');
 const fs = require('fs');
-const { GITHUB_TOKEN } = require('./secrets.js');
 
 const args = process.argv.slice(2);
 const repoOwner = args[0];
@@ -30,11 +30,15 @@ function contributorsAvatarURL(err, data, body) {
 }
 
 function getRepoContributors(owner, name, cb) {
+  if (!owner || !name) {
+    console.log('You must provide a repository owner and name in your request');
+    return;
+  }
   const options = {
     url: `https://api.github.com/repos/${owner}/${name}/contributors`,
     headers: {
       'User-Agent': 'request',
-      Authorization: GITHUB_TOKEN,
+      Authorization: process.env.GITHUB_TOKEN,
     },
   };
 
